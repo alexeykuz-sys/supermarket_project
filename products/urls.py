566d974f2582bf5d5a16dbcd4992cd8bea96265b
alexeykuz-sys <alexeykuz@gmail.com>
2021-04-23 12:48:28 +0100

    set up products page and created models of categories and products in db

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('', views.all_products, name='products'),
    
 ]
--- supermarket_project/settings.py
+++ supermarket_project/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'allauth.socialaccount.providers.facebook',
     'home',
+    'products',
 ]
 
 MIDDLEWARE = [
