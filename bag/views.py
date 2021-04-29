a55d5c4de3ae823673eb4d3c823b3a967cfe999f
alexeykuz-sys <alexeykuz@gmail.com>
2021-04-29 20:33:56 +0100

    created bag page and add delete bag items function

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
@@ -28,3 +28,33 @@ def add_to_bag(request, item_id):
     request.session['bag'] = bag
     
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """ Adjust the quantity of the specified product to the shopping bag """
+    
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+    
+    if quantity:
+    
+        bag[item_id] = quantity
+    else:
+        bag.pop(item_id)
+        
+
+    request.session['bag'] = bag
+    
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """Remove item from the shopping bag """
+    try:
+        bag = request.session.get('bag', {})
+        bag.pop(item_id)
+        
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+    except Exception as e:
+        return HttpResponse(status=500)
