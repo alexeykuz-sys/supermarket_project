2f0e0398301b61e2e5dae2c14e314f98b9f27ee2
alexeykuz-sys <alexeykuz@gmail.com>
2021-05-04 15:56:26 +0100

    added checkout

---
+++ bag/templatetags/bag_tools.py
@@ -0,0 +1,7 @@
+from django import template
+
+register = template.Library()
+
+@register.filter(name='calc_subtotal')
+def calc_subtotal(price, quantity):
+    return price * quantity
--- supermarket_project/settings.py
+++ supermarket_project/settings.py
@@ -51,6 +51,10 @@ INSTALLED_APPS = [
     'checkout',
     'django_countries',
     
+    #others
+    
+    'crispy_forms'
+    
     
 ]
 
@@ -66,6 +70,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'supermarket_project.urls'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -79,9 +85,14 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request', # required by allauth
                 'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
