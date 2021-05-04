7aed20ed7b702f5eb250d4b6816b9bf5b46aa0ec
alexeykuz-sys <alexeykuz@gmail.com>
2021-05-04 20:13:41 +0100

    created checkout stripe js and css and template

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form':order_form,
+        'stripe_public_key': 'pk_test_51IbNAFGOFACUw6CtTX0Qx7HIzeVHXnwSCkClhhvUg78b32EYQzLV80rdQAwh7lC20x4kmtrrda4pyp0dAErn1DZL009mEDoAV7',
+        'client_secret': 'test client secret'
     }
     
     return render(request, template, context)
--- supermarket_project/settings.py
+++ supermarket_project/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
