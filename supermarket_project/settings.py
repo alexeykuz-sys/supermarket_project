3b0fc74e12690c2005a6017b97c23e700da24cd0
alexeykuz-sys <alexeykuz@gmail.com>
2021-05-10 20:14:16 +0100

    changed region in aws settings

--- supermarket_project/settings.py
+++ supermarket_project/settings.py
@@ -199,7 +199,7 @@ if 'USE_AWS' in os.environ:
     }
     
     AWS_STORAGE_BUCKET_NAME = 'ms4-supermarket-project'
-    AWS_S3_REGION_NAME = 'eu-west-2'
+    AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
