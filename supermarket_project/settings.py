260aa6f2b02b24dd879e1be76d8f676d829a11ef
alexeykuz-sys <alexeykuz@gmail.com>
2021-05-10 20:09:16 +0100

    fixed spelling error in db settings

--- supermarket_project/settings.py
+++ supermarket_project/settings.py
@@ -133,6 +133,7 @@ WSGI_APPLICATION = 'supermarket_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
@@ -141,11 +142,9 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-
-
 
 
 
