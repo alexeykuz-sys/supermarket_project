1e733df5f6e31f19cf9cb74250ad36a91e4ee324
alexeykuz-sys <alexeykuz@gmail.com>
2021-05-10 19:37:17 +0100

    fixed debug settings

--- supermarket_project/settings.py
+++ supermarket_project/settings.py
@@ -102,6 +102,8 @@ TEMPLATES = [
     },
 ]
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
 AUTHENTICATION_BACKENDS = [
@@ -139,7 +141,7 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
 
