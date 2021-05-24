ae04dd2af98c0e985b87d146cb44d344625a8f1f
alexeykuz-sys <alexeykuz@gmail.com>
2021-05-24 14:19:37 +0100

    added email settings

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -89,7 +89,6 @@ class StripeWH_Handler:
                     town_or_city__iexact=shipping_details.address.city,
                     street_address1__iexact=shipping_details.address.line1,
                     street_address2__iexact=shipping_details.address.line2,
-                    county__iexact=shipping_details.address.state,
                     grand_total=grand_total,
                     original_bag=bag,
                     stripe_pid=pid,
@@ -118,7 +117,6 @@ class StripeWH_Handler:
                     town_or_city=shipping_details.address.city,
                     street_address1=shipping_details.address.line1,
                     street_address2=shipping_details.address.line2,
-                    county=shipping_details.address.state,
                     original_bag=bag,
                     stripe_pid=pid,
                 )
