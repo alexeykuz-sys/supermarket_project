a4302daf863a3cd60ab5118b802efcc988eb9662
alexeykuz-sys <alexeykuz@gmail.com>
2021-05-25 06:06:22 +0100

    correct background img path

--- checkout/views.py
+++ checkout/views.py
@@ -1,20 +1,22 @@
+import json
 from django.shortcuts import (
     render, redirect, reverse, get_object_or_404, HttpResponse
 )
+
 from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
-from .forms import OrderForm
-from .models import Order, OrderLineItem
+import stripe
 
 from products.models import Product
 from profiles.models import UserProfile
 from profiles.forms import UserProfileForm
 from bag.contexts import bag_contents
 
-import stripe
-import json
+from .models import Order, OrderLineItem
+from .forms import OrderForm
+
 
 
 @require_POST
